cf137c3577d65e3525077ac96d7994a9c159403a
root <nagaraja.hm.0001@gmail.com>
2022-01-20 21:05:17 +0100

    Update: submodule Det

--- LATEST/Det.cpp
+++ LATEST/Det.cpp
@@ -5,3 +5,12 @@ class_Det Det;
 FUNC(void, DET_CODE) class_Det::InitFunction(void){
 }
 
+FUNC(void, DET_CODE) class_Det::ReportError(void){
+}
+
+FUNC(void, DET_CODE) class_Det::Start(void){
+}
+
+FUNC(void, DET_CODE) class_Det::GetVersionInfo(void){
+}
+
--- LATEST/Det.h
+++ LATEST/Det.h
@@ -5,7 +5,10 @@
 
 class class_Det{
    public:
-      FUNC(void, DET_CODE) InitFunction(void);
+      FUNC(void, DET_CODE) InitFunction   (void);
+      FUNC(void, DET_CODE) ReportError    (void);
+      FUNC(void, DET_CODE) Start          (void);
+      FUNC(void, DET_CODE) GetVersionInfo (void);
 };
 
 extern class_Det Det;
