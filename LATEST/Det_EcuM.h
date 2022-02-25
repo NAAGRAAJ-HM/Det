835bba1af6d515e6a63beaca03375dfd19310013
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:08:02 +0100

    Update: submodule Det

--- LATEST/Det_EcuM.h
+++ LATEST/Det_EcuM.h
@@ -27,7 +27,8 @@ class interface_Det_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, DET_CODE) InitFunction(void);
+      virtual FUNC(void, DET_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, DET_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
