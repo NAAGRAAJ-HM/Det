9418c699d5e58a5174bea5e432d8d8cc694d1a72
root <nagaraja.hm.0001@gmail.com>
2022-03-31 12:04:49 +0200

    Update: AUTOSAR basic software service module Det

--- LATEST/Det.cpp
+++ LATEST/Det.cpp
@@ -37,10 +37,9 @@ class module_Det:
    public:
       module_Det(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
-      FUNC(void, _CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, _CONFIG_DATA, _APPL_CONST) lptrCfgModule
+      FUNC(void, DET_CODE) InitFunction(
+         CONSTP2CONST(CfgModule_TypeAbstract, DET_CONFIG_DATA, DET_APPL_CONST) lptrCfgModule
       );
-      FUNC(void, DET_CODE) InitFunction   (void);
       FUNC(void, DET_CODE) DeInitFunction (void);
       FUNC(void, DET_CODE) MainFunction   (void);
 };
@@ -77,23 +76,39 @@ VAR(module_Det, DET_VAR) Det(
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
 FUNC(void, DET_CODE) module_Det::InitFunction(
-   CONSTP2CONST(CfgDet_Type, CFGDET_CONFIG_DATA, CFGDET_APPL_CONST) lptrCfgDet
+   CONSTP2CONST(CfgModule_TypeAbstract, DET_CONFIG_DATA, DET_APPL_CONST) lptrCfgModule
 ){
-   if(NULL_PTR == lptrCfgDet){
+   if(E_OK == IsInitDone){
 #if(STD_ON == Det_DevErrorDetect)
       Det_ReportError(
       );
 #endif
    }
    else{
-// check lptrCfgDet for memory faults
+      if(NULL_PTR == lptrCfgModule){
+#if(STD_ON == Det_DevErrorDetect)
+         Det_ReportError(
+         );
+#endif
+      }
+      else{
+// check lptrCfgModule for memory faults
 // use PBcfg_Det as back-up configuration
+      }
+      IsInitDone = E_OK;
    }
-   Det.IsInitDone = E_OK;
 }
 
 FUNC(void, DET_CODE) module_Det::DeInitFunction(void){
-   Det.IsInitDone = E_NOT_OK;
+   if(E_OK != IsInitDone){
+#if(STD_ON == Det_DevErrorDetect)
+      Det_ReportError(
+      );
+#endif
+   }
+   else{
+      IsInitDone = E_NOT_OK;
+   }
 }
 
 FUNC(void, DET_CODE) module_Det::MainFunction(void){
