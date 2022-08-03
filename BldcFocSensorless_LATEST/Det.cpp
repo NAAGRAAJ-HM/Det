6c985bed6eda9ec2c833b7c1a54cb7357361b796
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-03 17:37:20 +0200

    Update: AUTOSAR basic software service module for default error tracer

---
+++ BldcFocSensorless_LATEST/Det.cpp
@@ -0,0 +1,184 @@
+/******************************************************************************/
+/* File   : Det.cpp                                                           */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "Det.hpp"
+#include "infDet_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define DET_AR_RELEASE_VERSION_MAJOR                                           4
+#define DET_AR_RELEASE_VERSION_MINOR                                           3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(DET_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible DET_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(DET_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible DET_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+CONSTP2VAR(infDetClient,  DET_VAR, DET_CONST) gptrDet               = &Det;
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_Det, DET_VAR) Det;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, DET_CODE) module_Det::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, DET_CONST,       DET_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   DET_CONFIG_DATA, DET_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == Det_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstDet_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == Det_DevErrorDetect)
+         Det_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == Det_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == Det_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  DET_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, DET_CODE) module_Det::DeInitFunction(
+   void
+){
+#if(STD_ON == Det_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == Det_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == Det_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  DET_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, DET_CODE) module_Det::MainFunction(
+   void
+){
+#if(STD_ON == Det_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == Det_InitCheck)
+   }
+   else{
+#if(STD_ON == Det_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  DET_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+#if(STD_ON == _ReSIM)
+#include <iostream>
+using namespace std;
+#else
+#endif
+
+FUNC(Std_TypeReturn, DET_CODE) module_Det::ReportError(
+      uint16 IdModule
+   ,  uint8  IdInstance
+   ,  uint8  IdApi
+   ,  uint8  IdError
+){
+#if(STD_ON == _ReSIM)
+   cout<<endl<<"Development error reported";
+   cout<<endl<<"IdModule   = "<<IdModule;
+   cout<<endl<<"IdInstance = "<<IdInstance;
+   cout<<endl<<"IdApi      = "<<IdApi;
+   cout<<endl<<"IdError    = "<<IdError;
+#else
+#endif
+   return E_OK;
+}
+
+void Det_ReportRuntimeError(
+   void
+){
+}
+
+void Det_ReportTransientFault(
+   void
+){
+}
+
+FUNC(void, DET_CODE) module_Det::Start(
+   void
+){
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
