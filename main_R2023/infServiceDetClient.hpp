5cebe0ad9bdf592f0979973da06f9bc5cf182342
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-19 12:08:06 +0100

    Update: AUTOSAR basic software service module for default error tracer

--- main_R2023/infServiceDetClient.hpp
+++ main_R2023/infServiceDetClient.hpp
@@ -1,6 +1,6 @@
 #pragma once
 /******************************************************************************/
-/* File   : infServiceDetClient.hpp                                                  */
+/* File   : infServiceDetClient.hpp                                           */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
