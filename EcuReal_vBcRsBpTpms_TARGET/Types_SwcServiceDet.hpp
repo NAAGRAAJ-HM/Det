51e568eeaa26454dda6aa5f4992ec508ff12a19f
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-11-20 15:49:56 +0100

    AUTOSAR Migration Activities - EcuReal_vBcRsBpTpms_TARGET - SwcServiceDet

---
+++ EcuReal_vBcRsBpTpms_TARGET/Types_SwcServiceDet.hpp
@@ -0,0 +1,35 @@
+#ifndef DET_TYPES_H
+#define DET_TYPES_H
+
+#include "Rte_Det_Type.hpp"
+
+#define DET_APIID_INIT                  0x00u
+#define DET_APIID_REPORTERROR           0x01u
+#define DET_APIID_START                 0x02u
+#define DET_APIID_GETVERSIONINFO        0x03u
+#define DET_APIID_REPORTRUNTIMEERROR    0x04u
+#define DET_APIID_REPORTTRANSIENTFAULT  0x05u
+#define DET_APIID_GETLASTBUFFERINDEX    0x06u
+#define DET_APIID_GETBUFFERENTRY        0x07u
+#define DET_E_PARAM_POINTER     0x01u
+#define DET_E_CANNOT_REPORT     0x01u
+#define DET_E_WRONG_MODULE      0x02u
+#define DET_E_WRONG_INSTANCE    0x03u
+#define DET_E_WRONG_API         0x04u
+#define DET_E_WRONG_ERROR       0x05u
+#define DET_E_UNAVAILABLE       0x01u
+
+typedef uint16 Det_BufferIndexType;
+
+typedef struct{
+   uint8 Dummy;
+}Type_CfgSwcServiceDet_st;
+
+typedef struct{
+   uint16 ModuleId;
+   uint8 InstanceId;
+   uint8 ApiId;
+   uint8 ErrorId;
+}Det_ErrorEntryBufferType;
+
+#endif
